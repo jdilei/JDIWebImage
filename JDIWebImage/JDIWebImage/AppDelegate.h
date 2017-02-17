a6efd3e4229ada8d8d43e11576c689c97c39b2ba
Jdi-lei <Jdi-lei@jdilei.local>
2017-02-17 13:10:34 +0800

    创建项目

---
+++ JDIWebImage/JDIWebImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JDIWebImage
+//
+//  Created by Jdi-lei on 17/2/17.
+//  Copyright © 2017年 Jdi-lei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JDIWebImage/JDIWebImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JDIWebImage
+//
+//  Created by Jdi-lei on 17/2/17.
+//  Copyright © 2017年 Jdi-lei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
